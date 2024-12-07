68968b0cd11a90774672063963ab8624291fc054
jhs512 <jangka512@gmail.com>
2024-12-07 23:48:24 +0900

    dependencis

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,8 +10,24 @@ repositories {
 }
 
 dependencies {
+    implementation("org.reflections:reflections:0.10.2")
+    implementation("ch.qos.logback:logback-classic:1.5.12")
+
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+
+    testImplementation("org.assertj:assertj-core:3.26.3")
+
+    testImplementation("io.rest-assured:rest-assured:5.5.0")
+
+    compileOnly("org.projectlombok:lombok:1.18.36")
+    annotationProcessor("org.projectlombok:lombok:1.18.36")
+
+    testCompileOnly("org.projectlombok:lombok:1.18.36")
+    testAnnotationProcessor("org.projectlombok:lombok:1.18.36")
+
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.18.2")
+    implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.18.2")
 }
 
 tasks.test {
